79aa9c3dc6e8f6eb592170506d558bc12c9a41a8
666Parzival666 <oven.igor@yandex.ru>
2023-06-23 01:24:56 +0600

    commented pagination

--- reviews.py
+++ reviews.py
@@ -2,11 +2,8 @@ from flask import Blueprint, render_template, request, redirect, flash, url_for
 from models import db, Book, Cover, Genre, Review, User
 from flask_login import current_user, login_required
 from bleach import clean
-# from flask_paginate import Pagination, get_page_args
-
 
 bp = Blueprint('reviews', __name__)
-
 
 @bp.route('/add_review/<int:book_id>', methods=['GET', 'POST'])
 @login_required
@@ -51,26 +48,13 @@ def my_reviews():
 @bp.route('/moderate')
 @login_required
 def moderate_reviews():
-    # Получаем номер текущей страницы и количество элементов на странице из запроса
-    # page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-
-    # Запрашиваем только рецензии со статусом 1, сортируем их по дате добавления в обратном порядке
-    reviews = Review.query.filter_by(status_id=1).order_by(Review.date_added.desc()).limit(per_page).offset(offset).all()
-
+    reviews = Review.query.filter_by(status_id=1).all()
     for review in reviews:
         user = User.query.get(review.user_id)
         book = Book.query.get(review.book_id)
         review.first_name = user.first_name
         review.book_title = book.title
         review.added_date = review.date_added.strftime('%d.%m.%Y %H:%M')
-
-    # Получаем общее количество рецензий со статусом 1
-    total_reviews = Review.query.filter_by(status_id=1).count()
-
-    # Создаем объект Pagination
-    # pagination = Pagination(page=page, per_page=per_page, total=total_reviews, css_framework='bootstrap4')
-
-    # return render_template('moderate.html', reviews=reviews, pagination=pagination)
     return render_template('moderate.html', reviews=reviews)
 
 
